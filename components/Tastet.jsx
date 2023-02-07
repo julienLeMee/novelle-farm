d7a25021aae8f3b0ed3536b0c0c3dada707faba4
Julien Le Mee <julemee@gmail.com>
2023-02-07 18:21:09 -0500

    added tastet recipes

--- components/Tastet.jsx
+++ components/Tastet.jsx
@@ -8,11 +8,31 @@ const Tastet = () => {
         <h2 className='uppercase text-3xl tracking-widest text-[#35441e] pb-8 text-center'>Redécouvrez le goût</h2>
         <h3 className='text-xl tracking-widest text-[#35441e] pb-8 text-center'>Nos idées recettes de la ferme</h3>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 p-4 h-full w-full'>
-          <TastetItem tastetImg='https://images.unsplash.com/photo-1471193945509-9ad0617afabf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' />
-          <TastetItem tastetImg='https://images.unsplash.com/photo-1505872342847-6dbb5e76cd31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' />
-          <TastetItem tastetImg='https://images.unsplash.com/photo-1575672336898-d8142e84a14e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' />
-          <TastetItem tastetImg='https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' />
-          <TastetItem tastetImg='https://images.unsplash.com/photo-1576072115035-5fe30e447e60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80' />
+          <TastetItem
+            tastetImg='https://images.unsplash.com/photo-1471193945509-9ad0617afabf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            title={'Pot-au-feu'}
+            url={'/'}
+          />
+          <TastetItem
+            tastetImg='https://images.unsplash.com/photo-1505872342847-6dbb5e76cd31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+            title={'Crumble'}
+            url={'/'}
+          />
+          <TastetItem
+            tastetImg='https://images.unsplash.com/photo-1575672336898-d8142e84a14e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+            title={'Soupe de citrouilles'}
+            url={'/'}
+          />
+          <TastetItem
+            tastetImg='https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+            title={'Carottes râpées'}
+            url={'/'}
+          />
+          <TastetItem
+            tastetImg='https://images.unsplash.com/photo-1576072115035-5fe30e447e60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
+            title={'Radis au vinaigre'}
+            url={'/'}
+          />
         </div>
       </div>
     </div>
